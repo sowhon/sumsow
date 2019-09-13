7a60aba20e72b822fcc2931e06b54669127248a0
sowhon <55269930+sowhon@users.noreply.github.com>
2019-09-13 11:39:05 +0530

    Add files via upload

---
+++ ADD.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int x,y,add;
+clrscr();
+printf("\nEnter x,y");
+scanf("%d %d",&x,&y);
+add=x+y;
+printf("x=%d",x);
+printf("y=%d",y);
+printf("\nadd=%d",add);
+}
